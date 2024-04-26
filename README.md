8c8f5adfdbff8e2f1b3a32a0494cebe55e6cb53d
Håkon <haakonu97@gmail.com>
2024-04-26 20:46:41 +0200

    feat: usePopoverControls

--- README.md
+++ README.md
@@ -3,12 +3,90 @@
 > Utilize the [native web Popover API](https://developer.mozilla.org/en-US/docs/Web/API/Popover_API) with React
 
 ```tsx
-<NativePopover
-  trigger={(props) => <button {...props}>Open tooltip</button>}
-  popover={(props) => (
-    <div {...props} className="my-popover">
-      Popover content
+import { NativePopover } from "native-popover-react";
+
+function App() {
+  return (
+    <NativePopover
+      trigger={(props) => (
+        <button {...props}>Open tooltip</button>
+      )}
+      popover={(props) => (
+        <div {...props} className="my-popover">
+          Popover content
+        </div>
+      )}
+    />
+  );
+}
+```
+
+## Controls
+
+```tsx
+import {
+  NativePopover,
+  usePopoverControls,
+} from "native-popover-react";
+
+function App() {
+  const popover = usePopoverControls<HTMLDivElement>();
+
+  return (
+    <div>
+      <button onClick={popover.toggle}>
+        {popover.isOpen ? "Close popover" : "Open popover"}
+      </button>
+      <NativePopover
+        control={popover.control}
+        popover={(props) => (
+          <div {...props} className="my-popover">
+            Popover content
+          </div>
+        )}
+      />
     </div>
-  )}
-/>
+  );
+}
+```
+
+## Reference
+
+```typescript
+function NativePopover<
+  T extends HTMLElement = HTMLElement
+>(props: {
+  /** Spread props on a container for the popover content. **/
+  popover: (
+    props: {
+      id: string;
+      popover: string;
+      ref?: React.MutableRefObject<T | null>;
+    },
+    closeProps: {
+      popovertarget: string;
+      popovertargetaction: string;
+    }
+  ) => React.ReactNode;
+  /** Props should be applied to a `button` or `input` element **/
+  trigger?: (props: {
+    popovertarget: string;
+  }) => React.ReactNode;
+  type?: "auto" | "manual";
+  id?: string;
+  /** Returned from `usePopoverControls().control` **/
+  control?: React.MutableRefObject<T | null>;
+}): ReactNode;
+
+function usePopoverControls<
+  T extends HTMLElement = HTMLElement
+>(options?: {
+  onStateChange?: (newState: "open" | "closed") => void;
+}): {
+  show: () => void;
+  hide: () => void;
+  toggle: () => void;
+  control: React.MutableRefObject<T | null>;
+  isOpen: boolean;
+};
 ```
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 let i = 0;
 function generateId() {
   return `popover-${i++}`;
 }
 
-export function NativePopover(props: {
-  trigger: (props: { popovertarget: string }) => React.ReactNode;
+export function NativePopover<
+  T extends HTMLElement = HTMLElement
+>(props: {
+  trigger?: (props: {
+    popovertarget: string;
+  }) => React.ReactNode;
   popover: (
     props: {
       id: string;
       popover: string;
+      ref?: React.MutableRefObject<T | null>;
     },
     closeProps: {
       popovertarget: string;
@@ -19,16 +24,23 @@ export function NativePopover(props: {
   ) => React.ReactNode;
   type?: "auto" | "manual";
   id?: string;
+  control?: React.MutableRefObject<T | null>;
 }) {
-  const id = React.useRef(generateId());
+  const id = useRef(generateId());
   if (props.id) {
     id.current = props.id;
   }
   return (
     <>
-      {props.trigger({ popovertarget: id.current })}
+      {props.trigger
+        ? props.trigger({ popovertarget: id.current })
+        : null}
       {props.popover(
-        { id: id.current, popover: props.type ?? "auto" },
+        {
+          id: id.current,
+          popover: props.type ?? "auto",
+          ref: props.control,
+        },
         {
           popovertarget: id.current,
           popovertargetaction: "hide",
@@ -36,4 +48,88 @@ export function NativePopover(props: {
       )}
     </>
   );
+}
+
+export function usePopoverControls<
+  T extends HTMLElement = HTMLElement
+>(
+  options: {
+    onStateChange?: (newState: "open" | "closed") => void;
+  } = {}
+) {
+  const ref = useRef<T | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const show = () => {
+    if (!supportsPopover()) {
+      return console.error(
+        "Popover API is not supported in your browser"
+      );
+    }
+    if (ref.current && "showPopover" in ref.current) {
+      ref.current.showPopover();
+    } else {
+      console.error(
+        "Target element is not mounted, or does not support popover API"
+      );
+    }
+  };
+
+  const hide = () => {
+    if (!supportsPopover()) {
+      return console.error(
+        "Popover API is not supported in your browser"
+      );
+    }
+    if (ref.current && "hidePopover" in ref.current) {
+      ref.current.hidePopover();
+    } else {
+      console.error(
+        "Target element is not mounted, or does not support popover API"
+      );
+    }
+  };
+
+  const toggle = () => {
+    if (!supportsPopover()) {
+      return console.error(
+        "Popover API is not supported in your browser"
+      );
+    }
+    if (ref.current && "togglePopover" in ref.current) {
+      ref.current.togglePopover();
+    } else {
+      console.error(
+        "Target element is not mounted, or does not support popover API"
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (ref.current) {
+      setIsOpen(ref.current.matches(":popover-open"));
+      ref.current.addEventListener("beforetoggle", (event) => {
+        if ("newState" in event && event.newState === "open") {
+          setIsOpen(true);
+          options.onStateChange?.("open");
+        }
+        if ("newState" in event && event.newState === "closed") {
+          setIsOpen(false);
+          options.onStateChange?.("closed");
+        }
+      });
+    }
+  }, []);
+
+  return {
+    show,
+    hide,
+    toggle,
+    control: ref,
+    isOpen,
+  };
+}
+
+function supportsPopover() {
+  return HTMLElement.prototype.hasOwnProperty("popover");
 }
